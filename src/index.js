75738ed6f7dcb9a763b5ef9f21636860ef8928bd
ramprits <ramprits-sdei@gmail.com>
2020-08-09 14:04:28 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./pages/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter, HashRouter } from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:1337/graphql",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <HashRouter>
-        <App />
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
       </HashRouter>
     </BrowserRouter>
   </React.StrictMode>,
